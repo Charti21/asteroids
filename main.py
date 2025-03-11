f41aff0e1e415dc42da002e5b91c507c59387811
Charti21 <3joshchartier@gmail.com>
2025-03-10 20:08:55 -0400

    B:Added the Clock variable, limiting FPS, and added new circleshape.py file

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def main() :
     print("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
+    Clock = pygame.time.Clock() #Create a Clock Instance
+    dt = 0
     while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        Clock.tick(60)
+        dt = (Clock.tick(60) / 1000)
     
     
 if __name__ == "__main__" :
